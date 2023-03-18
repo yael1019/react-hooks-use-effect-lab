a2b5d24c250987a54d3ab24edc70ec026649cbaf
Yael Harosh <yaelharosh01@gmail.com>
2023-03-18 13:25:05 -0400

    finish tests, did not pass tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if(timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    } 
+
+    const timerId = setTimeout(() => {
+        return setTimeRemaining((timeRemaining) => timeRemaining - 1)
+    }, 1000)
+    
+
+    return function() {
+      clearTimeout(timerId);
+    }
+
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
